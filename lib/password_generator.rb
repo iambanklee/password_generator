637b968ec3a9b88dafec177bf2362c40100e50b9
Bank Lee <iambanklee@gmail.com>
2024-08-04 16:01:40 +0100

    other option validations with shared_example

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -9,27 +9,47 @@ module PasswordGenerator
   UPPERCASE = ("A".."Z").to_a.freeze
   LOWERCASE = ("a".."z").to_a.freeze
   NUMBER = (0..9).to_a.freeze
-  SPECIAL = %w[@ % ! ? * ^ &]
+  SPECIAL = %w[@ % ! ? * ^ &].freeze
 
   def self.generate(length:, uppercase:, lowercase:, number:, special:)
+    password = ""
     errors = []
+    bucket = []
 
     errors << "length must be an integer" unless length.is_a?(Integer)
-    errors << "uppercase must be a boolean value" unless [true, false].include?(uppercase)
 
-    raise InvalidOption.new(errors.join(",")) unless errors.empty?
+    if [true, false].include?(uppercase)
+      bucket << upper_case_char
+    else
+      errors << "uppercase must be a boolean value"
+    end
 
-    bucket = [upper_case_char, lower_case_char, number_char, special_char]
-    password = ""
+    if [true, false].include?(lowercase)
+      bucket << lower_case_char
+    else
+      errors << "lowercase must be a boolean value"
+    end
+
+    if number.is_a?(Integer)
+      bucket << number_char
+    else
+      errors << "number must be an integer"
+    end
+
+    if special.is_a?(Integer)
+      bucket << special_char
+    else
+      errors << "special must be an integer"
+    end
+
+    raise InvalidOption, errors.join(",") unless errors.empty?
 
     (0...length).each do |_|
-      password += "#{bucket.sample.call}"
+      password += bucket.sample.call.to_s
     end
 
     password
   end
-
-  private
 
   def self.upper_case_char
     -> { UPPERCASE.sample }
--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe PasswordGenerator do
     expect(PasswordGenerator::VERSION).not_to be nil
   end
 
+  shared_examples "InvalidOption" do
+    it "raises InvalidOption error" do
+      expect { subject }.to raise_error(PasswordGenerator::InvalidOption)
+    end
+  end
+
   describe ".generate" do
     subject(:generate_password) { described_class.generate(length:, uppercase:, lowercase:, number:, special:) }
 
@@ -24,17 +30,31 @@ RSpec.describe PasswordGenerator do
       context "when length is not an integer" do
         let(:length) { "A" }
 
-        it "raises InvalidOption error" do
-          expect { generate_password }.to raise_error(PasswordGenerator::InvalidOption)
-        end
+        it_behaves_like "InvalidOption"
       end
 
       context "when uppercase is not a boolean" do
         let(:uppercase) { "A" }
 
-        it "raises InvalidOption error" do
-          expect { generate_password }.to raise_error(PasswordGenerator::InvalidOption)
-        end
+        it_behaves_like "InvalidOption"
+      end
+
+      context "when lowercase is not a boolean" do
+        let(:lowercase) { "A" }
+
+        it_behaves_like "InvalidOption"
+      end
+
+      context "when number is not an integer" do
+        let(:number) { "A" }
+
+        it_behaves_like "InvalidOption"
+      end
+
+      context "when special is not an integer" do
+        let(:special) { "A" }
+
+        it_behaves_like "InvalidOption"
       end
     end
   end
