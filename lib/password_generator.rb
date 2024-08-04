8e05d8807300f2ff777601cbb314853cab86d9e7
Bank Lee <iambanklee@gmail.com>
2024-08-04 15:20:33 +0100

    PasswordGenerator.generate

--- lib/password_generator.rb
+++ lib/password_generator.rb
@@ -4,5 +4,38 @@ require_relative "password_generator/version"
 
 module PasswordGenerator
   class Error < StandardError; end
-  # Your code goes here...
+
+  UPPERCASE = ("A".."Z").to_a.freeze
+  LOWERCASE = ("a".."z").to_a.freeze
+  NUMBER = (0..9).to_a.freeze
+  SPECIAL = %w[@ % ! ? * ^ &]
+
+  def self.generate(length:, uppercase:, lowercase:, number:, special:)
+    bucket = [upper_case_char, lower_case_char, number_char, special_char]
+    password = ""
+
+    (0...length).each do |_|
+      password += "#{bucket.sample.call}"
+    end
+
+    password
+  end
+
+  private
+
+  def self.upper_case_char
+    -> { UPPERCASE.sample }
+  end
+
+  def self.lower_case_char
+    -> { UPPERCASE.sample }
+  end
+
+  def self.number_char
+    -> { NUMBER.sample }
+  end
+
+  def self.special_char
+    -> { SPECIAL.sample }
+  end
 end
--- spec/password_generator_spec.rb
+++ spec/password_generator_spec.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
+require 'password_generator'
+
 RSpec.describe PasswordGenerator do
   it "has a version number" do
     expect(PasswordGenerator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".generate" do
+    subject(:generate_password) { described_class.generate(length:, uppercase:, lowercase:, number:, special:) }
+
+    let(:length) { 8 }
+    let(:uppercase) { true }
+    let(:lowercase) { true }
+    let(:number) { 0 }
+    let(:special) { 0 }
+
+    it "generates password in given length" do
+      expect(generate_password.size).to eq(length)
+    end
   end
 end
